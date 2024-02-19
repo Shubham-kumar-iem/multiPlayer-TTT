161bd8ae6d8b47bfd31e13da1cee9e9893b6255c
Shubham Kumar <shubhammuz110203@gmail.com>
2024-02-19 16:15:59 +0530

    UI ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './App.css'
+import Square from "./square/Square"
+
+const renderFrom = [[1, 2, 3 ], [4, 5, 6], [7, 8, 9]];
 
 const App = () => {
   return (
-    <div>
-      Shubham Kumar
+    <div className='main-div'>
+    <div className='move-detection'>
+        <div className='left'>yourSelf</div>
+        <div className='right'>Opponent</div>
+    </div>
+      <div>
+        <h1 className='heading water-bg'>Tic Tak Toe</h1>
+        <div className='square-wrapper'>
+            {
+                renderFrom.map((arr) => 
+                    arr.map((e) => {
+                        return <Square/>;
+                    })
+                )
+            }
+        </div>
+      </div>
     </div>
   )
 }
