f3154b22d5be26858783daa1966574fc94dcbb9f
Shubham Kumar <shubhammuz110203@gmail.com>
2024-02-19 21:03:58 +0530

    implemented rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,65 @@
-import React from 'react'
-import './App.css'
-import Square from "./square/Square"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Square from "./square/Square";
 
-const renderFrom = [[1, 2, 3 ], [4, 5, 6], [7, 8, 9]];
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
 const App = () => {
+  const [gameState, setGameState] = useState(renderFrom);
+  const [currentPlayer, setCurrentPlayer] = useState("circle");
+  const [finishedState, setFinishedState] = useState(false);
+
+  const checkWinner = () => {
+    for (let row = 0; row < gameState.length; row++) {
+      if(gameState[row][0] === gameState[row][1]  && gameState[row][1] === gameState[row][2]) {
+        return gameState[row][0];
+      }
+    }
+    for (let col = 0; col < gameState.length; col++) {
+      if(gameState[0][col] === gameState[1][col]  && gameState[1][col] === gameState[2][col]) {
+        return gameState[0][col];
+      }
+    }
+  };
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if(winner === 'circle' || winner === 'cross') {
+      setFinishedState(winner);
+    }
+  }, [gameState]);
+
   return (
-    <div className='main-div'>
-    <div className='move-detection'>
-        <div className='left'>yourSelf</div>
-        <div className='right'>Opponent</div>
-    </div>
+    <div className="main-div">
+      <div className="move-detection">
+        <div className="left">yourSelf</div>
+        <div className="right">Opponent</div>
+      </div>
       <div>
-        <h1 className='heading water-bg'>Tic Tak Toe</h1>
-        <div className='square-wrapper'>
-            {
-                renderFrom.map((arr) => 
-                    arr.map((e) => {
-                        return <Square/>;
-                    })
-                )
-            }
+        <h1 className="heading water-bg">Tic Tak Toe</h1>
+        <div className="square-wrapper">
+          {gameState.map((arr, rowIndex) =>
+            arr.map((e, colIndex) => {
+              return (
+                <Square
+                  finishedState={finishedState}
+                  currentPlayer={currentPlayer}
+                  setCurrentPlayer={setCurrentPlayer}
+                  setGameState={setGameState}
+                  id={rowIndex * 3 + colIndex}
+                  key={rowIndex * 3 + colIndex}
+                />
+              );
+            })
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/square/Square.jsx
+++ src/square/Square.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import './Square.css'
+import React, { useState } from "react";
+import "./Square.css";
 
-const Square = () => {
+const circleSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        stroke="#ffffff"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </g>
+  </svg>
+);
+const crossSvg = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M19 5L5 19M5.00001 5L19 19"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </g>
+  </svg>
+);
+
+const Square = ({ setGameState, id, currentPlayer, setCurrentPlayer, finishedState, setFinishedState }) => {
+  const [icon, setIcon] = useState(null);
+
+  const clickOnCircle = () => {
+    if (!icon) {
+      if (currentPlayer === "circle") {
+        setIcon(circleSvg);
+      } else {
+        setIcon(crossSvg);
+      }
+      const myCurrentPlayer = currentPlayer;
+      setCurrentPlayer(currentPlayer === "circle" ? "cross" : "circle");
+
+      setGameState(prevState => {
+        let newState = [...prevState];
+        const rowIndex = Math.floor(id / 3);
+        const colIndex = id % 3;
+        newState[rowIndex][colIndex] = myCurrentPlayer;
+        return newState;
+      });
+    }
+  };
+
   return (
-    <div className='square'></div>
-  )
-}
+    <div onClick={clickOnCircle} className={`square ${finishedState ? 'not-allowed' : null}`}>
+      {icon}
+    </div>
+  );
+};
 
-export default Square
+export default Square;
